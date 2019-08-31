136c01ef266ba2c4954b56c16a10ee056f80d0f3
류수한 <ryusoohan@naver.com>
2019-08-31 20:30:17 +0900

    [UPDATE] functions about loadbalancer had been fixed

--- src/main/java/eight/domain/balancer/RoundRobinNodeBalancer.java
+++ src/main/java/eight/domain/balancer/RoundRobinNodeBalancer.java
@@ -1,11 +1,44 @@
 package eight.domain.balancer;
 
+import eight.model.TargetGroup;
+import eight.repository.TargetGroupRepository;
+import eight.util.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
 class RoundRobinNodeBalancer implements NodeBalancer {
 
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
-	}
+	@Autowired
+	TargetGroupRepository targetGroupRepository;
 
+	@Autowired
+	ConnectionPool connectionPool;
+
+	List<TargetGroup> targetGroupList;
+
+	AtomicInteger count = new AtomicInteger(0);
+
+	@Override
+	public HttpServletResponse execute() {
+		// TODO Auto-generated method stub
+		targetGroupList = targetGroupRepository.findAll();
+
+		int index = count.getAndIncrement() % targetGroupList.size();
+
+		TargetGroup target = targetGroupList.get(index);
+
+		String destination = target.getHost()+":"+target.getInboundPort();
+
+		AtomicInteger con = connectionPool.getConnection(destination);
+		con.getAndIncrement();
+
+		System.out.println(con);
+
+		return null;
+	}
 }
